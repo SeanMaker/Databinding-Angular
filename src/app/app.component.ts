71cc3590fb96d416584427bb7f2a373dafcf7c62
Shiheng Dong <dongshiheng9@gmail.com>
2019-06-15 23:57:41 -0400

    upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular-Databinding';
+
+  serverElements = [{ type: 'server', name: 'Testserver', content: 'Just a test!'}];
+
+
+
+}
